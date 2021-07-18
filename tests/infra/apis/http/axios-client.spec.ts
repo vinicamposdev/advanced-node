e4f21e255602c16abba1f9bd953a9da69aff6eca
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-07-18 13:11:16 -0300

    feat: ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/apis/http/axios-client.spec.ts
+++ tests/infra/apis/http/axios-client.spec.ts
@@ -41,4 +41,12 @@ describe('AxiosHttpClient', () => {
 
     expect(result).toEqual('any_data')
   })
+
+  it('should rethrow if get throws', async () => {
+    fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+    const promise = sut.get({ url, params })
+
+    await expect(promise).rejects.toThrow(new Error('http_error'))
+  })
 })
