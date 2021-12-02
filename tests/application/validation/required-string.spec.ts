057b7eaf5a1f309987e1cb6d6f65fc6232b22574
vinicamposdev <viniciusmoliveirac@hotmail.com>
2021-12-02 17:04:34 -0300

    refactor: move producton code to their own files

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -1,17 +1,5 @@
 import { RequiredFieldError } from '@/application/errors'
-
-class RequiredStringValidator {
-  constructor (
-    private readonly value: string,
-    private readonly fieldName: string
-  ) {}
-
-  validate (): Error | undefined {
-    if (this.value === '' || this.value === undefined || this.value === null) {
-      return new RequiredFieldError(this.fieldName)
-    }
-  }
-}
+import { RequiredStringValidator } from '@/application/validation'
 
 describe('RequiredStringValidator', () => {
   it('should return Error if value is empty', () => {
