2777e9209573d2a78a8f3fe256675c3cc1f9d734
vinicamposdev <viniciusmoliveirac@hotmail.com>
2021-12-02 13:38:05 -0300

    test: ensure PgUserAccountRepository returns an account id

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -39,17 +39,18 @@ describe('PgUserAccountRepository', () => {
     })
   })
 
-  describe('save()', () => {
+  describe('saveWithFacebook()', () => {
     it('should create an account if id is undefined', async () => {
-      await sut.saveWithFacebook({
+      const account = await sut.saveWithFacebook({
         email: 'any_email',
         name: 'any_name',
         facebookId: 'any_fb_id'
       })
 
-      const account = await pgUserRepo.findOne({ email: 'any_email' })
+      const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
 
-      expect(account?.id).toBe(1)
+      expect(pgUser?.id).toBe(1)
+      expect(account?.id).toBe('1')
     })
 
     it('should create an account if id is defined', async () => {
@@ -58,7 +59,7 @@ describe('PgUserAccountRepository', () => {
         name: 'any_name',
         facebookId: 'any_fb_id'
       })
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         id: '1',
         email: 'new_email',
         name: 'new_name',
@@ -73,6 +74,7 @@ describe('PgUserAccountRepository', () => {
         name: 'new_name',
         facebookId: 'new_fb_id'
       })
+      expect(id).toBe('1')
     })
   })
 })
