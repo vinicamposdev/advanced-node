c4917541c6a981961b678cd64d59d9be9f5d57a9
vinicamposdev <viniciusmoliveirac@hotmail.com>
2021-12-03 16:21:26 -0300

    refactor: transform ExpressRouter class to a function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,13 +1,11 @@
 
 import { Controller } from '@/application/controllers'
 
-import { Request, Response } from 'express'
+import { RequestHandler } from 'express'
 
-export class ExpressRouter {
-  constructor (private readonly controller: Controller) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body })
+export const adaptExpressRoute = (controller: Controller): RequestHandler => {
+  return async (req, res) => {
+    const httpResponse = await controller.handle({ ...req.body })
     res.status(httpResponse.statusCode).json(
       httpResponse.statusCode === 200 ? httpResponse.data : { error: httpResponse.data.message }
     )
--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
+import { makeFacebookLoginController } from '@/main/factories/controllers'
+import { adaptExpressRoute } from '@/infra/http'
 
 export default (router: Router): void => {
-  router.post('/api/login/facebook', (req, res) => {
-    res.send({ data: 'any_data' })
-  })
+  router.post('/api/login/facebook', adaptExpressRoute(makeFacebookLoginController()))
 }
