a0fde5001158cb098145ca53c07b956337c619cf
vinicamposdev <viniciusmoliveirac@hotmail.com>
2021-12-04 17:56:49 -0300

    feat: ensure JwtTokenHandler returns the key used to sign

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -51,15 +51,24 @@ describe('JwtTokenHandler', () => {
   })
 
   describe('validateToken()', () => {
+    let key: string
     let token: string
     beforeAll(() => {
+      key = 'any_key'
       token = 'any_token'
+      fakeJwt.verify.mockImplementation(() => ({ key }))
     })
     it('should call sign with correct params', async () => {
       await sut.validateToken({ token })
 
       expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret)
       expect(fakeJwt.verify).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return the key used to sign', async () => {
+      const generatedKey = await sut.validateToken({ token })
+
+      expect(generatedKey).toBe(key)
     })
   })
 })
